c7f694b5ac15a9212fd16c36b2de72fec5f31d45
Benja1972 <benja@bk.ru>
2019-11-29 17:12:22 +0100

    some changes

--- python/play_with_sensors.py
+++ python/play_with_sensors.py
@@ -4,12 +4,13 @@ import pandas as pd
 
 filename = "punch.csv"
 
-df = pd.read_csv("/data/" + filename)
+df = pd.read_csv("../data/" + filename)
 
 index = range(1, len(df['aX']) + 1)
 
 plt.rcParams["figure.figsize"] = (20,10)
 
+plt.figure()
 plt.plot(index, df['aX'], 'g.', label='x', linestyle='solid', marker=',')
 plt.plot(index, df['aY'], 'b.', label='y', linestyle='solid', marker=',')
 plt.plot(index, df['aZ'], 'r.', label='z', linestyle='solid', marker=',')
@@ -17,8 +18,9 @@ plt.title("Acceleration")
 plt.xlabel("Sample #")
 plt.ylabel("Acceleration (G)")
 plt.legend()
-plt.show()
+#plt.show()
 
+plt.figure()
 plt.plot(index, df['gX'], 'g.', label='x', linestyle='solid', marker=',')
 plt.plot(index, df['gY'], 'b.', label='y', linestyle='solid', marker=',')
 plt.plot(index, df['gZ'], 'r.', label='z', linestyle='solid', marker=',')
