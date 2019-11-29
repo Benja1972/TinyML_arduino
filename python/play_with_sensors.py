1b47700420352b9925d14ba437d101b857416ba3
Benja1972 <benja@bk.ru>
2019-11-29 16:13:44 +0100

    add folders

---
+++ python/play_with_sensors.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+filename = "punch.csv"
+
+df = pd.read_csv("/data/" + filename)
+
+index = range(1, len(df['aX']) + 1)
+
+plt.rcParams["figure.figsize"] = (20,10)
+
+plt.plot(index, df['aX'], 'g.', label='x', linestyle='solid', marker=',')
+plt.plot(index, df['aY'], 'b.', label='y', linestyle='solid', marker=',')
+plt.plot(index, df['aZ'], 'r.', label='z', linestyle='solid', marker=',')
+plt.title("Acceleration")
+plt.xlabel("Sample #")
+plt.ylabel("Acceleration (G)")
+plt.legend()
+plt.show()
+
+plt.plot(index, df['gX'], 'g.', label='x', linestyle='solid', marker=',')
+plt.plot(index, df['gY'], 'b.', label='y', linestyle='solid', marker=',')
+plt.plot(index, df['gZ'], 'r.', label='z', linestyle='solid', marker=',')
+plt.title("Gyroscope")
+plt.xlabel("Sample #")
+plt.ylabel("Gyroscope (deg/sec)")
+plt.legend()
+plt.show()
